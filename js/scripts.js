84f4542cef46cfe868fbe66226a16fb035f0a346
Christopher Neal <christopher.m.neal@gmail.com>
2021-10-25 22:49:31 -0700

    Separate business logic from UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,54 @@
+// BUSINESS LOGIC
+function toArray(string, spacing) {
+  string = string.toLowerCase();
+  return string.split(spacing);
+}
 
+function noPunct(eachCharacterArray) {
+  const punctArray = [",", ".", ";", ":", "?", "!", "-"];
+  if (punctArray.includes(eachCharacterArray.at(-1))) {
+    eachCharacterArray.pop();
+  }
+  eachCharacterArray.forEach(function(char) {
+    if (punctArray.includes(char)) {
+      eachCharacterArray.splice(eachCharacterArray.indexOf(char), 1);
+    } 
+  });
+  return eachCharacterArray.join("")
+}
 
+function wordCountSort(arrayOfWords) {
+  arrayOfWords.sort();
+  lastCount = 0;
+  lastWord = "";
+  let wordCountArray = [];
+  arrayOfWords.forEach(function(word) {
+    if (word === lastWord) {
+      lastCount += 1;
+    } else {
+      wordCountArray.push([lastCount, lastWord]);
+      lastWord = word;
+      lastCount = 1;
+    }
+  });
+  wordCountArray.sort();
+  wordCountArray.reverse();
+  return wordCountArray;
+}
 
-// UI logic
+// UI LOGIC
 $(document).ready(function() {
   $("#text").submit(function(event) {
     event.preventDefault();
+
     let textString = $("#word-order-text").val();
-    textString = textString.toLowerCase();
-    let eachChar = textString.split("");
-    const punctArray = [",", ".", ";", ":", "?", "!", "-"];
-    if (punctArray.includes(eachChar.at(-1))) {
-      eachChar.pop();
-    }
-    eachChar.forEach(function(char) {
-      if (punctArray.includes(char)) {
-        console.log(char);
-        console.log(eachChar.indexOf(char));
-        eachChar.splice(eachChar.indexOf(char), 1);
-      } 
-    });
-    let newTextString = eachChar.join("")
-    console.log(newTextString);
-    let sentenceArray = newTextString.split(" ");
-    sentenceArray.sort();
-    lastCount = 0;
-    lastWord = "";
-    let wordCountArray = [];
-    sentenceArray.forEach(function(word) {
-      if (word === lastWord) {
-        lastCount += 1;
-      } else {
-        wordCountArray.push([lastCount, lastWord]);
-        // $("#result").append("<li>" + lastWord + " - " + lastCount);
-        lastWord = word;
-        lastCount = 1;
-      }
-    });
-    wordCountArray.sort();
-    wordCountArray.reverse()
-    wordCountArray.forEach(function(wordArray) {
-      $("#result").append("<li>" + wordArray[1] + " - " + wordArray[0] + "</li>");
+    let eachChar = toArray(textString, "");
+    let newTextString = noPunct(eachChar);
+    let wordArray = toArray(newTextString, " ")
+    let wordCountArray = wordCountSort(wordArray);
+
+    wordCountArray.forEach(function(numberWords) {
+      $("#result").append("<li>" + numberWords[1] + " - " + numberWords[0] + "</li>");
     });
   });
 });
