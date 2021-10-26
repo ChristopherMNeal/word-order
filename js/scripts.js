a277533f243f621f3d70fd6624389b558d41cbd4
Christopher Neal <christopher.m.neal@gmail.com>
2021-10-25 22:09:45 -0700

    Add word count function, add remove punctuation function

--- js/scripts.js
+++ js/scripts.js
@@ -7,15 +7,30 @@ $(document).ready(function() {
     event.preventDefault();
     let textString = $("#word-order-text").val();
     textString = textString.toLowerCase();
-    let sentenceArray = textString.split(" ");
-    sentenceArray.sort();
-    nestedSen = [];
-    sentenceArray.forEach(function(word) {
-      sentenceArray.filter(word => nestedSen.push(word));
+    let eachChar = textString.split("");
+    const punctArray = [",", ".", ";", ":", "?", "!", "-"];
+    let noPunctArray = [];
+    eachChar.forEach(function(char) {
+      if (punctArray.includes(char)) {
+        console.log(char);
+        console.log(eachChar.indexOf(char));
+        eachChar.splice(eachChar.indexOf(char), 1);
+      }
     });
-
-
-    const resultSentence = sentenceArray.join(" ");
-    $("#result").text(resultSentence);
+    let newTextString = eachChar.join("")
+    console.log(newTextString);
+    let sentenceArray = newTextString.split(" ");
+    sentenceArray.sort();
+    lastCount = 0;
+    lastWord = "";
+    sentenceArray.forEach(function(word) {
+      if (word === lastWord) {
+        lastCount += 1;
+      } else {
+        $("#result").append("<li>" + lastWord + " - " + lastCount);
+        lastWord = word;
+        lastCount = 1;
+      }
+    });
   });
 });
